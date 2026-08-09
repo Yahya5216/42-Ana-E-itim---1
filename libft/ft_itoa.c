3f4fc7f70f5d06744a3aa195315ae901e9d11544
Yahya Aydilek <yahyaaydilek40@gmail.com>
2026-08-10 00:58:01 +0300

    ywni gönderimlerimizi yaptık son 5 gün kaldı

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,9 +1,57 @@
 
 #include "libft.h"
 
+size_t	return_len(long n)
+{
+	size_t	len;
+
+	len = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		len++;
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+void	fill_to_char(long nb, char *d, size_t len)
+{
+	if (nb < 0)
+	{
+		nb *= -1;
+		d[0] = '-';
+	}
+	while (nb > 0)
+	{
+		d[--len] = (nb % 10) + '0';
+		nb = nb / 10 ;
+	}
+}
+
 char	*ft_itoa(int n)
 {
 	char	*d;
+	size_t	len;
+	long	nb;
 	
+	nb = n;
+	len = return_len(nb);
+	d = (char *)malloc((len + 1) * sizeof(char));
+	if (!d)
+		return (NULL);
+	d[len] = '\0';
+	if (nb == 0)
+	{
+		d[0] = '0';
+		return (d);
+	}
+	fill_to_char(nb, d, len);
 	return (d);
 }
