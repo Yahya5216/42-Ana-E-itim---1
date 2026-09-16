cbce01a5a88750a561698b542fe009360da09531
Yahya Aydilek <yahyaaydilek40@gmail.com>
2026-09-16 23:54:25 +0300

    python_module_00 daex06 ya kadar yaptım

---
+++ python_module_00/ft_count_harvest_iterative.py
@@ -0,0 +1,6 @@
+def ft_count_harvest_iterative():
+	day_count = int(input("Days until harvest: "))
+	start = 1
+	for start in range(1, day_count + 1):
+		print(f"Day {start}")
+	print("Harvest time!")
---
+++ python_module_00/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def count_days(start, day_count):
+	if start > day_count:
+		return
+	print(f"Day {start}")
+	count_days(start + 1, day_count)
+
+def ft_count_harvest_recursive():
+	
+	day_count = int(input("Days until harvest: "))
+	start = 1
+	count_days(start, day_count);
